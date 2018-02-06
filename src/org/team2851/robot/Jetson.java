bf06aca2ef294788fb8e9c3cd88fa34810f578ca
Jason Deal <jmd613@outlook.com>
2018-02-05 19:19:19 -0500

    Updated gitignore and added Jetson

--- src/org/team2851/robot/Jetson.java
+++ src/org/team2851/robot/Jetson.java
@@ -1,4 +1,16 @@
 package org.team2851.robot;
 
-public class Jetson {
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+public class Jetson
+{
+    private NetworkTableInstance networkTableInstance;
+    private NetworkTable networkTable;
+
+    public Jetson()
+    {
+        networkTableInstance = NetworkTableInstance.getDefault();
+        networkTable = networkTableInstance.getTable("Jetson");
+    }
 }
