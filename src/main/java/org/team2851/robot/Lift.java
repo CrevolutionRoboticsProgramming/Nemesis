079c34bd1477a1659b1841c0c0c21b8f387a6fad
jmd613 <jason.deal101@gmail.com>
2018-03-22 10:47:10 -0400

    Simplified Lift

--- src/main/java/org/team2851/robot/Lift.java
+++ src/main/java/org/team2851/robot/Lift.java
@@ -19,25 +19,8 @@ public class Lift extends Subsystem
     private Lift() { super("Lift"); }
     private double currentPower = 0.0D;
 
-    public enum LiftPosition
-    {
-        INTAKE(0.0, 0),
-        MOVING(10.5, 1),
-        Switch(20.0, 2),
-        SCALE_LOW(40.0, 3),
-        SCALE_HIGH(60.0, 4),
-        CLIMB(45.0, 5);
-
-        public final double height;
-        public final int index;
-        LiftPosition(double height, int index) { this.height = height; this.index = index; }
-    }
-
     private TalonSRX talon;
     private final double circumference = 2.5;
-    private LiftPosition endAutonPos;
-
-    private int heightToCounts(double height) { return (int)(height * circumference * 4096); }
 
     @Override
     public void init()
@@ -101,25 +84,13 @@ public class Lift extends Subsystem
     public Command getTeleopCommand() {
         return new Command()
         {
-            // a : up
-            // b : down
-            // temp (time) : 0.5 seconds
             private Controller c = Robot.copilot;
-            private LatchedBoolean a = new LatchedBoolean();
-            private LatchedBoolean b = new LatchedBoolean();
-            private Timer t = new Timer();
-            private int pos = 0;
-            private double power;
 
             @Override
             public boolean isFinished() { return false; }
 
             @Override
-            public void start()
-            {
-                if (endAutonPos != null) pos = endAutonPos.index;
-                power = Preferences.getInstance().getDouble("Power", 0);
-            }
+            public void start() { }
 
             @Override
             public void update()
@@ -128,106 +99,16 @@ public class Lift extends Subsystem
                 else if (c.b.getState()) talon.set(ControlMode.PercentOutput, -.5);
                 else if (c.x.getState()) talon.set(ControlMode.PercentOutput, 0.15); // Stalls motor (Keeps lift up)
                 else talon.set(ControlMode.PercentOutput, 0);
-
-                System.out.println("Lift Current Draw: " + talon.getOutputCurrent());
-//                if (c.leftTrigger.getValue() > 0.3)
-//                {
-//                    if (c.a.getState()) talon.set(ControlMode.PercentOutput, 0.1);
-//                    else if (c.b.getState()) talon.set(ControlMode.PercentOutput, -0.1);
-//                    return;
-//                } else if (a.getValue(c.a.getState()))
-//                {
-//                    if (pos == 0)
-//                    {
-//                        logError("Cannot lower lift. Lift@Pos:0");
-//                        return;
-//                    }
-//                    pos--;
-//                } else if (b.getValue(c.b.getState()))
-//                {
-//                    if (pos == 5)
-//                    {
-//                        logError("Cannot raise lift. Lift@Pos:MAX");
-//                        return;
-//                    }
-//                    pos++;
-//                }
-//
-//                switch (pos)
-//                {
-//                    case 0:
-//                        talon.set(ControlMode.Position, heightToCounts(LiftPosition.INTAKE.height));
-//                        break;
-//                    case 1:
-//                        talon.set(ControlMode.Position, heightToCounts(LiftPosition.MOVING.height));
-//                        break;
-//                    case 2:
-//                        talon.set(ControlMode.Position, heightToCounts(LiftPosition.Switch.height));
-//                        break;
-//                    case 3:
-//                        talon.set(ControlMode.Position, heightToCounts(LiftPosition.SCALE_LOW.height));
-//                        break;
-//                    case 4:
-//                        talon.set(ControlMode.Position, heightToCounts(LiftPosition.SCALE_HIGH.height));
-//                        break;
-//                    case 5:
-//                        talon.set(ControlMode.Position, heightToCounts(LiftPosition.CLIMB.height));
-//                        break;
-//                    default:
-//                        logError("Set to invalid height.");
-//                }
             }
 
             @Override
-            public void done()
-            {
-//                talon.set(ControlMode.PercentOutput, 0);
-//                t.stop();
-                currentPower = 0;
-            }
+            public void done() { currentPower = 0; }
 
             @Override
             public void interrupt() { done(); }
 
             @Override
             public String getName() { return "Teleop"; }
-        };
-    }
-
-    public Command setPosition(LiftPosition position)
-    {
-        return new Command()
-        {
-            boolean isFinished = false;
-            @Override
-            public boolean isFinished() {
-                return false;
-            }
-
-            @Override
-            public void start() {
-
-            }
-
-            @Override
-            public void update() {
-                talon.set(ControlMode.Position, position.height);
-            }
-
-            @Override
-            public void done() {
-                talon.set(ControlMode.PercentOutput, 0);
-            }
-
-            @Override
-            public void interrupt() {
-                done();
-            }
-
-            @Override
-            public String getName() {
-                return "SetPosition[" + position.toString() + "]";
-            }
         };
     }
 
